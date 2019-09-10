42aeea10a2999f854ec9c917eecd95cceb8f7e1c
kepasa05 <rromero@andaluciajunta.es>
2019-09-10 12:44:32 +0200

    Eliminado puntuacion inicio, se agrega contador para que realice 10 preguntas

--- TablasMultiplicar.java
+++ TablasMultiplicar.java
@@ -9,15 +9,16 @@ public class TablasMultiplicar {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		int points = 10;
+		int puntos = 0;
+		int contador = 0;
 		int tablaMultiplicar, number, multi;
 		int resultado = 0;
 		
 		System.out.println ("Introduzca la tabla de multiplicar que quieres practicar: ");
 		tablaMultiplicar = sc.nextInt();
 		
-		while (points >= 10 && points <=100 ) {
-			System.out.println("La puntuacion hasta ahora es: " + points);
+		while (contador < 9 && puntos <=100 ) {
+			System.out.println("La puntuacion hasta ahora es: " + puntos);
 			
 			number = (int) (Math.random()* 10);
 			System.out.println ("¿ Resultado de multiplicar " + tablaMultiplicar + " x " + number + " ?");
@@ -25,21 +26,21 @@ public class TablasMultiplicar {
 			resultado = sc.nextInt();
 			
 			if (resultado == multi) {
-				points+=10;
+				puntos+=10;
 				
 			} else {
-				points-=5;
+				puntos-=5;
 			}
-			
+			contador+=1;
 		}
 		
-		System.out.println ("Su puntuacion final es: " + points);
+		System.out.println ("Su puntuacion final es: " + puntos);
 		
-		if (points >=110) {
+		if (puntos >=110) {
 			System.out.println("Ha obtenido una medalla de ORO ");
-		} else if (points >=90) {
+		} else if (puntos >=90) {
 			System.out.println("Ha obtenido una medalla de PLATA ");
-		} else if (points >= 80) {
+		} else if (puntos >= 80) {
 			System.out.println("Ha obtenido una medalla de BRONCE ");
 		} else {
 			System.out.println("No ha obtenido ninguna medalla, HAY QUE PRACTICAR MASSSSS ");
