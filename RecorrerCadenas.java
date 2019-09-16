f98efe11ce2e9b36475842ae71119a2dbec6a828
kepasa05 <rromero@andaluciajunta.es>
2019-09-16 10:56:17 +0200

    Nuevo Fichero practicar cadenas

--- RecorrerCadenas.java
+++ RecorrerCadenas.java
@@ -7,22 +7,26 @@ public class RecorrerCadenas {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		char contenidoCaracter;
+		char contadorCaracteres;
+		String cadenaUsuarioConcatenar;
+		String cadenaConcatenada = "";
 		
 		Scanner sc = new Scanner(System.in);
 		sc.useDelimiter("\n");
-
+	
+			
 		System.out.println("Introduzca una cadena de caracteres: ");
-		String cadenaCaracter = sc.next();
+		cadenaUsuarioConcatenar = sc.next();
 		
-		System.out.println("La cadena introducida tiene una longitud de " + + cadenaCaracter.length() + " caracteres" );
-		
-		for (int i = 0; i < cadenaCaracter.length();i++) {
-			contenidoCaracter = cadenaCaracter.charAt(i);
-			System.out.println(contenidoCaracter);
+		while (!cadenaUsuarioConcatenar.isEmpty()) {
+			
+			cadenaConcatenada += cadenaUsuarioConcatenar; 
+			
+			System.out.println("Introduzca otra cadena de caracteres: ");
+			cadenaUsuarioConcatenar = sc.next();
 		}
 		
-		
+		System.out.println("Las cadenas concatenadas son: " + cadenaConcatenada);
 		sc.close();
 		
 		
